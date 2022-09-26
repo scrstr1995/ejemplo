d1e97798a24f298508829e63c8d38de3f6c82a2a
unknown <luiseduardopg09@gmail.com>
2022-09-26 11:24:02 -0500

    Estructura del proyecto

---
+++ src/components/LegoMain.vue
@@ -0,0 +1,22 @@
+<template>
+<LegoHeader/>
+
+</template>
+
+<script>
+import LegoHeader from './LegoHeader'
+export default {
+  name: 'LegoMain',
+  props: {
+
+    }
+    ,components:{
+      LegoHeader
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
